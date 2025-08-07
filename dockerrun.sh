2e6d8bfff66468f9daf658a7b03de36aab2e266a
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-08-07 16:38:30 +0100

    selenium

--- dockerrun.sh
+++ dockerrun.sh
@@ -28,4 +28,27 @@ curl -k http://localhost/API/SystemAPI?method=runPendingDatabaseUpdates
 
 crontab /etc/cron.d/cron
 
+# --- Selenium IDE Config Setup ---
+
+SELENIUM_DIR="/usr/local/aspen-discovery/tests/e2e/selenium-ide/sites"
+SITE_NAME="test.localhostaspen"
+SITE_DIR="${SELENIUM_DIR}/${SITE_NAME}"
+CONFIG_FILE="${SITE_DIR}/${SITE_NAME}-test-config.json"
+TEMPLATE_FILE="${SELENIUM_DIR}/example/example-test-config.json"
+
+# Create the config file if it doesn't exist
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Generating Selenium IDE config for $SITE_NAME"
+  mkdir -p "$SITE_DIR"
+  cp "$TEMPLATE_FILE" "$CONFIG_FILE"
+  chmod a+rw "$CONFIG_FILE"
+fi
+
+# --- Replace placeholders in the config file ---
+sed -i "s/\"siteName\": \"example\"/\"siteName\": \"${SITE_NAME}\"/g" "$CONFIG_FILE"
+sed -i "s|\"url\": \"https://example.library.org\"|\"url\": \"http://${SITE_NAME}\"|g" "$CONFIG_FILE"
+sed -i "s/\"username\": \"1234567\"/\"username\": \"42\"/g" "$CONFIG_FILE"
+sed -i "s/\"password\": \"example_password\"/\"password\": \"koha\"/g" "$CONFIG_FILE"
+sed -i "s/\"invalidPassword\": \"wrong_password\"/\"invalidPassword\": \"wrongpass\"/g" "$CONFIG_FILE"
+
 /bin/bash -c "trap : TERM INT; sleep infinity & wait"
